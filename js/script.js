329821ecd6d7f76a24a6184b6195171e2cd9cea0
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-03-28 18:19:43 +0000

    copied template into all files

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  alert('Hello, World!')
+}
