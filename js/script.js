d8dd2ade250c1e4a5c8c5df35a648a6151645560
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-02 18:08:38 +0000

    finished with js calculations and css

--- js/script.js
+++ js/script.js
@@ -7,5 +7,15 @@
 "use strict"
 
 function myButtonClicked() {
-  alert('Hello, World!')
+//input
+  const firstBase = document.getElementById("base-a").value
+  const secondBase = document.getElementById("base-b").value
+  const heightOfTrapezium = document.getElementById("height").value
+//process
+  const areaHalfway = (firstBase * secondBase) * heightOfTrapezium
+  const areaAnswer = areaHalfway / 2
+
+//output
+  document.getElementById("answer").innerHTML =
+  "The area of this trapezium is: " + areaAnswer + " mm²."
 }
